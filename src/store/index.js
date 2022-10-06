15b8341b2be4777e45873cdb1d16fff85cc9cbda
anuraga091 <anuraga091@gmail.com>
2022-10-06 08:46:22 +0530

    Simple redux counter application for learning

---
+++ src/store/index.js
@@ -0,0 +1,42 @@
+// import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {counter: 0},
+    reducers : {
+        increment(state, action) {
+            state.counter++        },
+        decrement(state, action){
+            state.counter--
+        },
+        incrementByTen(state, action){
+            state.counter += action.payload   
+        }
+    }
+})
+
+export const actions = counterSlice.actions;
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+
+export default store;
+// const reducerFn = (state={counter: 0}, action) => {
+//     //Should always be synchronous 
+//     //should not mutate the original state
+//     if (action.type === 'INC'){
+//         return {counter: state.counter + 1}
+//     }
+//     if (action.type === 'DEC'){
+//         return {counter: state.counter - 1}
+//     }
+//     if (action.type === 'Add') {
+//         return {counter: state.counter + action.payload}
+//     }
+//     return state
+// }
+// const store = createStore(reducerFn)
+// export default store;
+
